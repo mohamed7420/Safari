7d8fd689e879f93c119eeca38087eb06d90b799c
mohamedosama2074 <iosengks74@gmail.com>
2019-09-29 20:22:41 +0200

    SafariServices

--- Safari/ViewController.swift
+++ Safari/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+    }
+    
+    @IBAction func btn_show(_ sender: UIButton) {
+    
+        showSafariServices("https://www.facebook.com/profile.php?id=100013381152500")
+    
+    }
+    
+    func showSafariServices(_ url:String){
+        
+        guard let url = URL(string: url) else {return}
+        
+        
+        let safariVC = SFSafariViewController(url: url)
+        
+        present(safariVC, animated: true, completion: nil)
+        
     }
 
 
